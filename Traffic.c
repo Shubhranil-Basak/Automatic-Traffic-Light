a41a8e1005e2e30888df606c01a5a3d5e04ef1a4
Shubhranil Basak <144095577+Shubhranil-Basak@users.noreply.github.com>
2023-10-31 14:21:27 +0530

    Update Traffic.c

--- Traffic.c
+++ Traffic.c
@@ -35,7 +35,9 @@ void print(){
 	    }
     }
     for(int i = 20, j = 1; i <= 23; i++, j++){
-        printf("%d Yellow light right at side %d\n", hardware[i], j);
+	    if(hardware[i]){
+		    printf("%d Yellow light right at side %d\n", hardware[i], j);
+	    }
     }
     for(int i = 24, j = 1; i <= 27; i++, j++){
         printf("%d Green light straight at side %d\n", hardware[i], j);
