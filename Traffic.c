936c161acd6ed73f37f11adb57d4e8b876c022a9
Shubhranil Basak <144095577+Shubhranil-Basak@users.noreply.github.com>
2023-10-31 14:17:55 +0530

    Update Traffic.c

--- Traffic.c
+++ Traffic.c
@@ -20,8 +20,9 @@ for(int i = 0; i < 32; i++){
 
 void print(){
     for(int i = 8, j = 1; i <= 11; i++, j++){
-		if(hardware[i])
-	        printf("Red light straight at side %d is on\n", j);
+		if(hardware[i]){
+	        	printf("Red light straight at side %d is on\n", j);
+		}
     }
     for(int i = 12, j = 1; i <= 15; i++, j++){
         printf("%d Red light right at side %d\n", hardware[i], j);
